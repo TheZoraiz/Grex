0ff95abf79ccda5709ae285e6854b2413dade283
Zoraiz Hassan <hzoraiz8@gmail.com>
2022-05-27 20:26:37 +0500

    SSL setup

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,16 @@ const mongoose = require('mongoose')
 
 const config = require('./config')
 
-const http = require('http')
-const httpServer = http.createServer(app)
+const https = require('https')
+const path = require('path')
+const fs = require('fs')
+const httpServer = https.createServer(
+    {
+      key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+      cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+    },
+    app
+)
 const { Server } = require('socket.io')
 
 let io = new Server(httpServer, {
